366b0c10d904b0bc02b684f915c012f2dc6c0633
scofield701 <jokerdarknight66@ezweb.ne.jp>
2019-05-02 13:22:09 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,160 +1,122 @@
 User.create!(
-  email: 'user1@gmail.com',
+  {email: 'user1@gmail.com',
   password: '123456',
   nickname: 'ケイスケ',
   intro: '圭介です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/keisuke.jpg")
-)
-User.create!(
-  email: 'user2@gmail.com',
+  image: open("#{Rails.root}/db/img/keisuke.jpg")},
+  {email: 'user2@gmail.com',
   password: '123456',
   nickname: 'ジュンヤ',
   intro: '純也です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/junya.jpg")
-)
-User.create!(
-  email: 'user3@gmail.com',
+  image: open("#{Rails.root}/db/img/junya.jpg")},
+  {email: 'user3@gmail.com',
   password: '123456',
   nickname: 'ユキ',
   intro: '由紀です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/yuki.jpg")
-)
-User.create!(
-  email: 'user4@gmail.com',
+  image: open("#{Rails.root}/db/img/yuki.jpg")},
+  {email: 'user4@gmail.com',
   password: '123456',
   nickname: 'ナオコ',
   intro: '菜生子です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/naoko.jpg")
-)
-User.create!(
-  email: 'user5@gmail.com',
+  image: open("#{Rails.root}/db/img/naoko.jpg")},
+  {email: 'user5@gmail.com',
   password: '123456',
   nickname: 'コウダイ',
   intro: '宏大です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/koudai.jpg")
-)
-User.create!(
-  email: 'user6@gmail.com',
+  image: open("#{Rails.root}/db/img/koudai.jpg")},
+  {email: 'user6@gmail.com',
   password: '123456',
   nickname: 'ヒロツグ',
   intro: '浩継です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/hirotugu.jpg")
-)
-User.create!(
-  email: 'user7@gmail.com',
+  image: open("#{Rails.root}/db/img/hirotugu.jpg")},
+  {email: 'user7@gmail.com',
   password: '123456',
   nickname: 'エヴァ',
   intro: 'エヴァです',
   sex: 1,
-  image: open("#{Rails.root}/db/img/eva.jpeg")
-)
-User.create!(
-  email: 'user8@gmail.com',
+  image: open("#{Rails.root}/db/img/eva.jpeg")},
+  {email: 'user8@gmail.com',
   password: '123456',
   nickname: 'ナオミ',
   intro: '直美です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/naomi.jpg")
-)
-User.create!(
-  email: 'user9@gmail.com',
+  image: open("#{Rails.root}/db/img/naomi.jpg")},
+  {email: 'user9@gmail.com',
   password: '123456',
   nickname: 'アシュリー',
   intro: 'アシュリーです',
   sex: 1,
-  image: open("#{Rails.root}/db/img/asyuri.jpg")
-)
-User.create!(
-  email: 'user10@gmail.com',
+  image: open("#{Rails.root}/db/img/asyuri.jpg")},
+  {email: 'user10@gmail.com',
   password: '123456',
   nickname: 'かなこ',
   intro: '夏菜子です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/kanako.jpeg")
-)
-User.create!(
-  email: 'user11@gmail.com',
+  image: open("#{Rails.root}/db/img/kanako.jpeg")},
+  {email: 'user11@gmail.com',
   password: '123456',
   nickname: 'けんじろう',
   intro: '健二郎です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/kenjirou.jpg")
-)
-User.create!(
-  email: 'user12@gmail.com',
+  image: open("#{Rails.root}/db/img/kenjirou.jpg")},
+  {email: 'user12@gmail.com',
   password: '123456',
   nickname: 'ジョン',
   intro: 'ジョンです',
   sex: 0,
-  image: open("#{Rails.root}/db/img/jon.jpg")
-)
-User.create!(
-  email: 'user13@gmail.com',
+  image: open("#{Rails.root}/db/img/jon.jpg")},
+  {email: 'user13@gmail.com',
   password: '123456',
   nickname: 'なぎさ',
   intro: '渚です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/nagisa.png")
-)
-User.create!(
-  email: 'user14@gmail.com',
+  image: open("#{Rails.root}/db/img/nagisa.png")},
+  {email: 'user14@gmail.com',
   password: '123456',
   nickname: 'ありさ',
   intro: '亜里沙です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/arisa.jpg")
-)
-User.create!(
-  email: 'user15@gmail.com',
+  image: open("#{Rails.root}/db/img/arisa.jpg")},
+  {email: 'user15@gmail.com',
   password: '123456',
   nickname: 'がく',
   intro: '学です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/gaku.jpg")
-)
-User.create!(
-  email: 'user16@gmail.com',
+  image: open("#{Rails.root}/db/img/gaku.jpg")},
+  {email: 'user16@gmail.com',
   password: '123456',
   nickname: 'つよし',
   intro: '剛です',
   sex: 0,
-  image: open("#{Rails.root}/db/img/tuyosi.jpeg")
-)
-User.create!(
-  email: 'user17@gmail.com',
+  image: open("#{Rails.root}/db/img/tuyosi.jpeg")},
+  {email: 'user17@gmail.com',
   password: '123456',
   nickname: 'メイ',
   intro: 'メイです',
   sex: 1,
-  image: open("#{Rails.root}/db/img/mei.png")
-)
-User.create!(
-  email: 'user18@gmail.com',
+  image: open("#{Rails.root}/db/img/mei.png")},
+  {email: 'user18@gmail.com',
   password: '123456',
   nickname: 'カーラ',
   intro: 'カーラです',
   sex: 1,
-  image: open("#{Rails.root}/db/img/kara.jpg")
-)
-User.create!(
-  email: 'user19@gmail.com',
+  image: open("#{Rails.root}/db/img/kara.jpg")},
+  {email: 'user19@gmail.com',
   password: '123456',
   nickname: 'アン',
   intro: 'アンです',
   sex: 1,
-  image: open("#{Rails.root}/db/img/an.jpg")
-)
-User.create!(
-  email: 'user20@gmail.com',
+  image: open("#{Rails.root}/db/img/an.jpg")},
+  {email: 'user20@gmail.com',
   password: '123456',
   nickname: 'ともみ',
   intro: '智美です',
   sex: 1,
-  image: open("#{Rails.root}/db/img/tomomi.jpg")
+  image: open("#{Rails.root}/db/img/tomomi.jpg")},
 )
